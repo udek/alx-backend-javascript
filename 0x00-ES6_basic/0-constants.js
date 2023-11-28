bfe830c49069f25f1aad4a04f03e911ce011803f
Martins Udekwe <martinsudek4@gmail.com>
2023-11-28 01:09:26 +0100

    Const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,13 @@
+export const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+};
+
+export const getLast = () => ' is okay';
+
+export const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+};
